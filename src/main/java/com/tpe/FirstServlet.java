075dfc8f0a96a614b1ce39b9c94b8ab2ef82a378
FikretArslan <arslanfikret33@gmail.com>
2022-06-25 11:24:14 +0300

    25/06/2022

---
+++ src/main/java/com/tpe/FirstServlet.java
@@ -0,0 +1,47 @@
+package com.tpe;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "FirstServlet", value = "/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Init");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Destroy");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+     //   response.getWriter().append("Served at: ").append(request.getContextPath());
+        System.out.println("Context Patth:"+request.getContextPath());
+        System.out.println("Servlet Path"+request.getServletPath());
+        System.out.println(request.getParameter("name"));
+
+        String name=request.getParameter("name");
+        response.getWriter().append("Served at: ").append("My name is "+name);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+
+
+}
